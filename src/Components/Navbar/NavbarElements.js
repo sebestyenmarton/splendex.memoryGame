7e997116d7136d7adbb0729ca49ba2a075f490cd
sdgmarton <sdgmarton@gmail.com>
2021-05-19 17:45:52 +0300

    3. Commit

--- src/Components/Navbar/NavbarElements.js
+++ src/Components/Navbar/NavbarElements.js
@@ -1,3 +1,4 @@
+import { IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
@@ -5,7 +6,7 @@ export const Nav = styled.nav`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 70px;
+  height: 50px;
   //margin-top: -70px;
   top: 0;
   z-index: 100;
@@ -18,17 +19,95 @@ export const NavbarContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   max-width: 1100px;
-  height: 70px;
+  height: 50px;
   width: 100%;
   padding: 7px 24px;
   z-index: 1;
   font-weight: 600;
-  color: #fff;
+  color: #f4f4f4;
 `;
 
 export const NavbarLogo = styled.img` 
   height: 100%;
   width: initial;
   border-radius: 3px;
-  padding-left: 24px;
+  padding: 0 24px;
+`;
+
+export const SetGameContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: -128px;
+  @media screen and (max-width: 650px){
+    margin: 0;
+  }
+`;
+
+export const DeckText = styled.div`
+  padding-right: 1rem;
+  font-size: 0.7rem;
+  color: #b4b4b4;
+`;
+
+export const DeckSize = styled.button`
+  margin-right: 1rem;
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding: 0 12px;
+  background: #fff;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+`;
+
+export const DeckSlidebar = styled.ul`
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+  box-shadow: 0 0 10px rgba(0,0,0,.2);
+  border-radius: 3px;
+  position: absolute;
+  top: 43px;
+  margin-left: -110px;
+  display: none;
+`;
+
+export const DeckLink = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  list-style: none;
+  height: 56px;
+  width: 63.2px;
+  color: #1f1f1f;
+  font-weight: 100;
+`;
+
+export const SetGameButton = styled.button`
+  margin-right: 1rem;
+  height: 100%;
+  padding: 0 12px;
+  background: #ff3000;
+  color: #f4f4f4;
+  font-weight: bold;
+  border: none;
+  border-radius: 3px;
+  text-transform: uppercase;
+  cursor: pointer;
+`;
+
+export const DropDown = styled(IoMdArrowDropdown)`
+  margin-left: 6px;
+  font-size: 1.2rem;
+  display: block;
+`;
+
+export const DropUp = styled(IoMdArrowDropup)`
+  margin-left: 6px;
+  font-size: 1.2rem;
+  display: none;
 `;
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,16 +1,51 @@
+import $ from 'jquery';
 import React from 'react';
 import Logo from '../../Images/evista.png';
 import {
+  DeckLink,
+  DeckSize,
+  DeckSlidebar,
+  DeckText,
+  DropDown,
+  DropUp,
   Nav,
   NavbarContainer,
-  NavbarLogo
+  NavbarLogo,
+  SetGameButton,
+  SetGameContainer
 } from './NavbarElements';
 
-const Navbar = () => {
+const Navbar = ({}) => {
+  $(document).ready(function(){
+    $('#DeckSize').click(function(){
+      $('#DropDown').css("display","none");
+      $('#DropUp, #DeckSlidebar').css("display","block");
+    });
+  });
+
   return (
   <Nav>
-    <NavbarContainer>
+    <NavbarContainer> 
       <NavbarLogo src={Logo} />
+      <SetGameContainer>
+        <DeckText> Deck size: </DeckText>
+          <DeckSize id="DeckSize"> 
+            20 
+            <DropDown id="DropDown" />
+            <DropUp id="DropUp" />
+          </DeckSize>
+          <DeckSlidebar id="DeckSlidebar">
+            <DeckLink>6</DeckLink>
+            <DeckLink>8</DeckLink>
+            <DeckLink>10</DeckLink>
+            <DeckLink>12</DeckLink>
+            <DeckLink>14</DeckLink>
+            <DeckLink>16</DeckLink>
+            <DeckLink>18</DeckLink>
+            <DeckLink>20</DeckLink>
+          </DeckSlidebar>
+        <SetGameButton>start new game</SetGameButton>
+      </SetGameContainer>
     </NavbarContainer>
   </Nav>
   );
